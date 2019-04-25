99180bdc1bd6f6334ff62b2a6f69bad45ab7b27c
nayakam <1308012+nayakam@users.noreply.github.com>
2019-04-25 12:19:13 +1000

    Hello World in Kotlin

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # kotlin-playground
+
+
+
+# Reference
+*   
+*   [https://play.kotlinlang.org](https://play.kotlinlang.org)
+*   [koans](https://play.kotlinlang.org/koans/overview)
---
+++ src/intro/helloV1.kt
@@ -0,0 +1,21 @@
+package intro
+
+fun main(args: Array<String>) {
+    val name = if (args.size > 0) args[0] else "Kotlin"
+    println("Hello, $name !!!")
+    println("Hello, ${args.getOrNull(0)} !!!")
+
+    /** https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/get-or-else.html */
+    val elseCase = args.getOrElse(0) { " Case else" }
+    println("Hello, $elseCase !!!")
+
+    val nameCheck = if (args.isNullOrEmpty()) " Null or Emtpy" else args[0]
+    println("Hello, $nameCheck !!!")
+
+    println("First ${foo()}, second ${foo()}")
+}
+
+fun foo(): String {
+    println("Calculating foo...")
+    return "foo"
+}
